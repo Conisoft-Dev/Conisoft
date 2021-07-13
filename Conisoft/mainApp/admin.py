0aac91adc1b7a49e0c564da1342d500f6b899cda
ShayCerny <shaycerny20@gmail.com>
2021-07-12 17:25:20 -0700

    Added Worshop model, registered to admin pannel

--- Conisoft/mainApp/admin.py
+++ Conisoft/mainApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Project,Member,Service,Timeline
+from .models import Project,Member,Service,Timeline, Worshop
 
 # Register your models here.
 admin.site.register(Timeline)
 admin.site.register(Service)
 admin.site.register(Project)
 admin.site.register(Member)
+admin.site.register(Worshop)
+
--- Conisoft/mainApp/models.py
+++ Conisoft/mainApp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Worshop(models.Model):
+    name = models.CharField(max_length=200)
+    presenter = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    open_slots = models.IntegerField()
+    taken_slots = models.IntegerField(default=0)
+    zoom_link = models.CharField(blank=True, max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+# Unused models v
 class Project(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
