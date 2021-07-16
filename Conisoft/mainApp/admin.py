f7913b81690066022c3c7125995970c53e4e8ec1
ShayCerny <shaycerny20@gmail.com>
2021-07-15 17:48:10 -0700

    Cleaned up logos, removed un-used models

--- Conisoft/mainApp/admin.py
+++ Conisoft/mainApp/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import Project,Member,Service,Timeline, Worshop
+from .models import Worshop
 
 # Register your models here.
-admin.site.register(Timeline)
-admin.site.register(Service)
-admin.site.register(Project)
-admin.site.register(Member)
 admin.site.register(Worshop)
 
