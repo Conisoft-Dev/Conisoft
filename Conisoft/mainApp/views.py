d64196ec87c292851db8a8ea6007df477071e421
charles <charleshparmley@icloud.com>
2021-08-06 15:18:27 -0500

    forms

--- Conisoft/mainApp/views.py
+++ Conisoft/mainApp/views.py
@@ -2,6 +2,11 @@ from django.views.generic import TemplateView, ListView
 from .models import Edition, Topic, PaperRequirement, Course, Carosel
 from django.shortcuts import render
 
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 class HomeView(ListView):
     edition_model = Edition
@@ -19,3 +24,17 @@ class HomeView(ListView):
 
 class RegisterView(TemplateView):
     template_name = 'register.html'
+
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="main/register.html", context={"register_form":form})
